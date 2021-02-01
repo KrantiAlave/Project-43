e82de6ed0732f55092b330153ae7dea8cc2183c7
Maithili Alave <73110690+KrantiAlave@users.noreply.github.com>
2021-02-01 21:50:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,36 @@
+//to create variables
 var hr,mn,sc;
 var hr_angle,mn_angle,sc_angle;
 
 function setup() {
+  //to create canvas
   createCanvas(400,400);
 }
 
 function draw() {
+  //to color background
   background(0); 
-
+  
+  //calculating time using predefined fun from p5.js
   hr = hour();
   mn = minute();
   sc = second();
-
+  
+  //measure angle in degrees
   angleMode(DEGREES);
-
+  
+  //compute the angle for the seconds, minutes and hour
   hr_angle = map(hr%12,0,12,0,360);
   mn_angle = map(mn,0,60,0,360);
   sc_angle = map(sc,0,16,0,360);
-
+  
+  //hands could easily be drawn 
   translate(200,200);
   
+  //to rotate the clock 
   rotate(-90);
   
+  //draw the clock hands
   push();
   noFill();
   strokeWeight(9);
@@ -49,5 +58,6 @@ function draw() {
   line(0,0,75,0);
   pop();
 
+  //to create draw sprites
   drawSprites();
 }
